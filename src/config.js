2ced72e252da21930e67a3ef2e5a81fcff457b71
Mountain/\Ash <mountainash@rmwpublishing.net>
2023-12-24 01:47:48 +0100

    👷‍♂️ fix: JS substring update

--- src/config.js
+++ src/config.js
@@ -64,7 +64,7 @@ const setDynamicVars = () => {
 		context.ACTOR = github.context.actor
 		context.REF = github.context.ref
 		context.SHA = github.context.sha
-		context.BRANCH = github.context.ref.substr(11)
+		context.BRANCH = github.context.ref.substring(11)
 	}
 }
 
